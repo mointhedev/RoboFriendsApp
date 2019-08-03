d09c32e4d85c1ff696801a42458d7710ef751d4e
mointhedev <52953600+moin248@users.noreply.github.com>
2019-08-03 22:26:42 +0500

    Updates

---
+++ precache-manifest.80f657dd677f3eacb6fa065eaad67bf5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5744d2de51b379050d0c73563d12e350",
+    "url": "/robofriends-app/index.html"
+  },
+  {
+    "revision": "1bb74c38e4c4d9133653",
+    "url": "/robofriends-app/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "c54d8b405203e29bed69",
+    "url": "/robofriends-app/static/css/main.96f34ba6.chunk.css"
+  },
+  {
+    "revision": "1bb74c38e4c4d9133653",
+    "url": "/robofriends-app/static/js/2.e93fd954.chunk.js"
+  },
+  {
+    "revision": "c54d8b405203e29bed69",
+    "url": "/robofriends-app/static/js/main.a85af1b9.chunk.js"
+  },
+  {
+    "revision": "e44d24ad0a2645503a25",
+    "url": "/robofriends-app/static/js/runtime~main.12706f03.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-app/static/media/SEGA.12a3db8b.woff"
+  }
+]);

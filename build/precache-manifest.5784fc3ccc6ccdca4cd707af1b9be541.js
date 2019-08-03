01c63048d2b8029880ad625a9a792aa60beed4ee
mointhedev <52953600+moin248@users.noreply.github.com>
2019-08-03 18:56:42 +0500

    making it my homepage

---
+++ build/precache-manifest.5784fc3ccc6ccdca4cd707af1b9be541.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f204411567b5ba3b4f46112592b001f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1bb74c38e4c4d9133653",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "5fb3dd8abb87dd1be5d8",
+    "url": "/static/css/main.e1878255.chunk.css"
+  },
+  {
+    "revision": "1bb74c38e4c4d9133653",
+    "url": "/static/js/2.e93fd954.chunk.js"
+  },
+  {
+    "revision": "5fb3dd8abb87dd1be5d8",
+    "url": "/static/js/main.a992f5fb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
